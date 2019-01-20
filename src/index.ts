6900202b93b74d4e3fd1c1b0d640b4f06cf6f84d
Ari Höysniemi <ari.hoysniemi@gmail.com>
2019-01-20 12:42:22 +0200

    Return string on print for testing.

--- src/index.ts
+++ src/index.ts
@@ -172,13 +172,14 @@ export const getLogStr = (
  * @param {string} tag - Tag to be used, if any.
  * @param {IglobalLogOptions} options - Custom options, if any.
  * @param {Date} date - Date, if any.
+ * @returns {string} - The full message that was printed.
  */
 export const print = (
   msg: any,
   tag?: string,
   options?: IglobalLogOptions,
   date?: Date
-): void => {
+): string => {
   try {
     const opt = options ? readLocalLogOptions(options) : globalLogOptions;
     const pStr = tag && tag !== '' ? `[${tag}]` : '';
@@ -187,8 +188,10 @@ export const print = (
     const m = ('0' + d.getMinutes()).slice(-2);
     const s = ('0' + d.getSeconds()).slice(-2);
     console.log(`${opt.printColor}${pStr}[${h}:${m}:${s}] -\x1b[0m`, msg);
+    return msg;
   } catch {
     console.log('');
+    return '';
   }
 };
 
